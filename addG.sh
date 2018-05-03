c1916f9eaf4c7390c82548fa6375a6dd553947eb
Kyle Scharnhorst <kyle.scharnhorst@continuant.com>
2018-05-03 13:05:12 -0700

    Add: update git completion file for g.
    amends the necessary lines if they don't exist.

--- addG.sh
+++ addG.sh
@@ -10,17 +10,65 @@
 # branch name when using g.
 #
 
+#############
+# VARIABLES #
+#############
+
 GIT_PATH=`which git.exe`
 GIT_DIR=$(dirname ${GIT_PATH})
 G_PATH=$GIT_DIR/g.exe
+COMPLETION_PATH=/mingw32/share/git/completion/git-completion.bash
+G_COMPLETE_STR="__git_complete g __git_main"
+GIT_COMPLETE_STR="__git_complete git __git_main"
+GEXE_COMPLETE_STR="__git_complete g.exe __git_main"
+GITEXE_COMPLETE_STR="__git_complete git.exe __git_main"
+
+#############
+# FUNCTIONS #
+#############
+
+function AddLineToCompletion {
+  if grep -q "$1" $COMPLETION_PATH
+  then
+    echo Found $1 no need to add
+  else
+    echo Placing $1 in $COMPLETION_PATH
+    sed -i "s/$2/$1\n$2/" $COMPLETION_PATH
+  fi
+}
+
+###################
+# START OF SCRIPT #
+###################
+
+# Print vars
+echo VARS:
+echo ------
 echo $GIT_PATH
 echo $GIT_DIR
 echo $G_PATH
+echo ------
+echo
 
+# Create g symlink
 if [ ! -f $G_PATH ]; then
   ln -s ${GIT_PATH} $G_PATH
+
+  if [ ! -f $G_PATH ]; then
+    echo Successfully created $G_PATH
+  fi
 else
   echo $G_PATH already exists
 fi
 
+# Amend lines to completion file
+if [ -f $COMPLETION_PATH ]; then
+  # backup completion file with epoch timestamp
+  cp $COMPLETION_PATH $COMPLETION_PATH.$(date +%s).bak
 
+  # see if completion file already has amended lines
+  AddLineToCompletion "$G_COMPLETE_STR" "$GIT_COMPLETE_STR"
+  AddLineToCompletion "$GEXE_COMPLETE_STR" "$GITEXE_COMPLETE_STR"
+else
+  echo Could not find git completion file at: $COMPLETION_PATH
+fi
