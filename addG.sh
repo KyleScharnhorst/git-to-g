566b388c15f8dda7590365ccf161cb93f7113e78
Kyle Scharnhorst <kyle.scharnhorst@continuant.com>
2018-05-03 10:02:00 -0700

    Add: inital addG.sh file.

---
+++ addG.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 
+# Run this script to setup a symlink to git so that you can call g instead of
+# git. This script also adds g to the git-completion.bash file so that
+# completion features work. I.e., hitting <tab><tab> to populate push or some
+# branch name when using g.
+#
+
+
